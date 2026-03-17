0964ee4c28b51c749c41839c9d5529160793011f
Lux Aeterna <luxaeternaizs@gmail.com>
2026-03-17 08:32:17 +0000

    9 workflow runs pages build and deployment pages-build-deployment #9: by

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/studio',
-  assetPrefix: '/studio',
+  assetPrefix: '/studio/',
   trailingSlash: true,
   images: {
     unoptimized: true,
